68ce132a7edbf7a1b4eae926fc21b5bbfd5146e9
thinhnk55 <thinhnk_55@vnu.edu.vn>
2023-08-17 08:50:37 +0700

    - alter bank table

--- sql/bank.sql
+++ sql/bank.sql
@@ -1,13 +1,12 @@
 CREATE TABLE IF NOT EXISTS timo_account
 (
     id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    username VARCHAR(50) UNIQUE NOT NULL ,
+    username VARCHAR(64) UNIQUE NOT NULL ,
     password VARCHAR(2048) NOT NULL,
-    device VARCHAR(50),
+    device VARCHAR(64),
     token VARCHAR(2048),
-    transaction_reference VARCHAR(50), #Ma giao dich cuoi cung truoc khi logout
-    logout_time BIGINT, #Ma giao dich cuoi cung truoc khi logout
     bank_account_id BIGINT,
+    detail VARCHAR(4096),
     state INT NOT NULL DEFAULT 0
 );
 
@@ -15,26 +14,26 @@ CREATE TABLE IF NOT EXISTS bank_account
 (
     id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     bank_code INT NOT NULL DEFAULT 0,
-    account_number VARCHAR(50),
-    account_owner VARCHAR(50),
+    account_number VARCHAR(32),
+    account_owner VARCHAR(256),
     state INT NOT NULL DEFAULT 0,
-    refer_id BIGINT NOT NULL
+    bank_detail_id BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bank_transaction
 (
     id                    BIGINT       PRIMARY KEY AUTO_INCREMENT,
-    bank_transaction_id   VARCHAR(50)              NOT NULL,
+    bank_transaction_id   VARCHAR(64)              NOT NULL,
     receiver_bankcode     INT          DEFAULT 0   NOT NULL,
-    receiver_bank_account VARCHAR(50)  DEFAULT ''  NOT NULL,
-    receiver_bank_name    VARCHAR(200) DEFAULT ''  NOT NULL,
-    receiver_name         VARCHAR(50)  DEFAULT ''  NOT NULL,
-    note                  TEXT         DEFAULT ''  NOT NULL,
+    receiver_bank_account VARCHAR(64)  DEFAULT ''  NOT NULL,
+    receiver_bank_name    VARCHAR(256) DEFAULT ''  NOT NULL,
+    receiver_name         VARCHAR(64)  DEFAULT ''  NOT NULL,
+    note                  VARCHAR(256)         DEFAULT ''  NOT NULL,
     amount                BIGINT       DEFAULT 0   NOT NULL,
     sender_bankcode       INT          DEFAULT 0   NOT NULL,
-    sender_bank_account   VARCHAR(50)  DEFAULT ''  NOT NULL,
-    sender_bank_name      VARCHAR(252) DEFAULT ''  NOT NULL,
-    sender_name           VARCHAR(252) DEFAULT ''  NOT NULL,
+    sender_bank_account   VARCHAR(64)  DEFAULT ''  NOT NULL,
+    sender_bank_name      VARCHAR(256) DEFAULT ''  NOT NULL,
+    sender_name           VARCHAR(256) DEFAULT ''  NOT NULL,
     create_time           BIGINT       DEFAULT 0   NOT NULL,
     status                INT          DEFAULT 0   NOT NULL,
     star_transaction_id   BIGINT       DEFAULT -1  NOT NULL
