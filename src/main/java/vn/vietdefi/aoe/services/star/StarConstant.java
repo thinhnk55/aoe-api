e0171a70683370dd4ab19d8ade81cf074aacfd4a
thinhnk55 <thinhnk_55@vnu.edu.vn>
2023-08-18 11:31:25 +0700

    - continue

--- src/main/java/vn/vietdefi/aoe/services/star/StarConstant.java
+++ src/main/java/vn/vietdefi/aoe/services/star/StarConstant.java
@@ -2,12 +2,14 @@ package vn.vietdefi.aoe.services.star;
 
 public class StarConstant {
     public static final int SERVICE_STAR_RECHARGE = 1;
-    public static final int SERVICE_DONATE_MATCH = 2;
-    public static final int SERVICE_DONATE_GAMER = 3;
-    public static final int SERVICE_DONATE_CASTER = 4;
-    public static final int SERVICE_DONATE_LEAGUE = 5;
-    public static final int SERVICE_SUGGEST_MATCH = 6;
-    public static final int SERVICE_REFUND = 7;
+    public static final int SERVICE_REFUND = 2;
+    public static final int SERVICE_SUGGEST_MATCH = 3;
+
+    public static final int SERVICE_DONATE_MATCH = 10;
+    public static final int SERVICE_DONATE_USER = 11;
+    public static final int SERVICE_DONATE_LEAGUE = 13;
+
+
 
     public static int DEFAULT_RECORD_PER_PAGE = 20;
 }
