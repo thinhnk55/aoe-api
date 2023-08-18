823df24efbe72fc728553d8dbac40475099f3226
thinhnk55 <thinhnk_55@vnu.edu.vn>
2023-08-18 15:19:57 +0700

    - aoe_match_donate

--- src/main/java/vn/vietdefi/aoe/services/donate/DonateService.java
+++ src/main/java/vn/vietdefi/aoe/services/donate/DonateService.java
@@ -1,16 +1,36 @@
 package vn.vietdefi.aoe.services.donate;
 
 import com.google.gson.JsonObject;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import vn.vietdefi.common.BaseResponse;
+import vn.vietdefi.util.log.DebugLogger;
+import vn.vietdefi.util.sql.HikariClients;
+import vn.vietdefi.util.sql.SQLJavaBridge;
 
 public class DonateService implements IDonateService{
     @Override
     public JsonObject createDonateMatch(JsonObject data) {
-        return null;
+        try{
+            SQLJavaBridge bridge = HikariClients.instance().defaulSQLJavaBridge();
+            bridge.insertObjectToDB("aoe_match_donate", data);
+            return BaseResponse.createFullMessageResponse(0, "success", data);
+        }catch (Exception e){
+            DebugLogger.error(ExceptionUtils.getStackTrace(e));
+            return BaseResponse.createFullMessageResponse(1, "system_error");
+        }
     }
 
     @Override
     public JsonObject getDonateMatchById(long id) {
-        return null;
+        try{
+            SQLJavaBridge bridge = HikariClients.instance().defaulSQLJavaBridge();
+            String query = "SELECT * FROM aoe_match_donate WHERE id = ?";
+            JsonObject data = bridge.queryOne(query, id);
+            return BaseResponse.createFullMessageResponse(0, "success", data);
+        }catch (Exception e){
+            DebugLogger.error(ExceptionUtils.getStackTrace(e));
+            return BaseResponse.createFullMessageResponse(1, "system_error");
+        }
     }
 
     @Override
