f256b8745f2b26342616e0abca1c6f70a6613a91
dinhtran271 <tranvandinh237@gmail.com>
2023-08-18 15:06:36 +0700

    add table  aoe_donate_gamer

--- sql/aoe_donate.sql
+++ sql/aoe_donate.sql
@@ -9,3 +9,19 @@ CREATE TABLE IF NOT EXISTS aoe_match_donate (
     message VARCHAR(256) NOT NULL,
     create_time BIGINT  NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS aoe_donate_gamer (
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    sender_id BIGINT NOT NULL,
+    sender_user_name VARCHAR(256) NOT NULL,
+    sender_nick_name VARCHAR(256) NOT NULL,
+    sender_phone VARCHAR(256) NOT NULL,
+    amount INT NOT NULL,
+    message VARCHAR(256) NOT NULL,
+    service int NOT NULL,           #11: donate for gamer,   12: donate for caster
+    receiver_id BIGINT NOT NULL,
+    receiver_user_name VARCHAR(256) NOT NULL,
+    receiver_nick_name VARCHAR(256) NOT NULL,
+    receiver_phone VARCHAR(256) NOT NULL,
+    create_time BIGINT  NOT NULL
+);
