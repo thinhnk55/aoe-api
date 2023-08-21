9ae9dfc247e45558942be26528ad72bf47d769ce
Khanhsof <Khanhak541@gmail.com>
2023-08-21 16:44:45 +0700

    fix sql , gamer

---
+++ src/main/java/vn/vietdefi/aoe/vertx/router/gamer/GamerApi.java
@@ -0,0 +1,62 @@
+package vn.vietdefi.aoe.vertx.router.gamer;
+
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import vn.vietdefi.aoe.vertx.router.clan.ClanRouter;
+import vn.vietdefi.api.vertx.ApiConfig;
+import vn.vietdefi.api.vertx.router.AuthRouter;
+
+public class GamerApi {
+    public static void configAPI(Router router) {
+        userAuthApi(router);
+        supportAuthApi(router);
+        adminAuthApi(router);
+        superAdminAuthApi(router);
+        systemAdminAuthApi(router);
+    }
+
+    public static void userAuthApi(Router router) {
+        router.get(ApiConfig
+                        .instance()
+                        .getPath("/gamer/get"))
+                .handler(GamerRouter::getGamerByUserId);
+        router.get(ApiConfig.instance().getPath("/gamer/list-of-match"))
+                .handler(GamerRouter::listGamerByMatchId);
+        router.get(ApiConfig.instance().getPath("/gamer/list"))
+                .handler(GamerRouter::listGamer);
+        router.get(ApiConfig.instance().getPath("/gamer/list-of-clan"))
+                .handler(GamerRouter::listGamerOfClan);
+        router.get(ApiConfig.instance().getPath("/gamer/match"))
+                .handler(GamerRouter::listMatch);
+    }
+
+    public static void supportAuthApi(Router router) {
+
+    }
+
+    public static void adminAuthApi(Router router) {
+        router.post(ApiConfig
+                        .instance()
+                        .getPath("/gamer/create"))
+                .handler(BodyHandler.create(false))
+                .handler(AuthRouter::authorizeAdmin)
+                .handler(GamerRouter::create);
+        router.post(ApiConfig
+                        .instance()
+                        .getPath("/gamer/update"))
+                .handler(BodyHandler.create(false))
+                .handler(AuthRouter::authorizeAdmin)
+                .handler(GamerRouter::updateInfo);
+    }
+
+    public static void superAdminAuthApi(Router router) {
+
+    }
+
+    public static void systemAdminAuthApi(Router router) {
+        router.post(ApiConfig.instance().getPath("/clan/create"))
+                .handler(BodyHandler.create(false))
+                .handler(AuthRouter::authorizeSystemAdmin)
+                .handler(ClanRouter::deleteClan);
+    }
+}
