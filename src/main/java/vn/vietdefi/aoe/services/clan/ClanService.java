fb17056f8a81caabb1f1c2f02d4a40397b2c32f4
thinhnk55 <thinhnk_55@vnu.edu.vn>
2023-08-21 16:49:30 +0700

    - fix clan

--- src/main/java/vn/vietdefi/aoe/services/clan/ClanService.java
+++ src/main/java/vn/vietdefi/aoe/services/clan/ClanService.java
@@ -10,23 +10,14 @@ import vn.vietdefi.util.sql.HikariClients;
 import vn.vietdefi.util.sql.SQLJavaBridge;
 
 public class ClanService implements IClanService{
-    public JsonObject createClan(JsonObject json){
+    public JsonObject createClan(JsonObject data){
         try {
             SQLJavaBridge bridge = HikariClients.instance().defaulSQLJavaBridge();
-            JsonObject data = new JsonObject();
-            String clanName = json.get("nick_name").getAsString();
-            data.addProperty("nick_name",clanName);
-            data.addProperty("full_name",json.get("full_name").getAsString());
+            String clanName = data.get("nick_name").getAsString();
             String query = "SELECT nick_name FROM aoe_clan WHERE nick_name = ?";
             if(bridge.queryExist(query,clanName)) {
                 return BaseResponse.createFullMessageResponse(12,"clan_name_exist");
             }
-            data.add("detail",json.get("detail"));
-            data.addProperty("avatar",json.get("avatar").getAsString());
-            data.addProperty("founder",json.get("founder").getAsString());
-            data.addProperty("owner_unit",json.get("owner_unit").getAsString());
-            data.addProperty("sport",json.get("sport").getAsString());
-            data.addProperty("create_day",json.get("create_day").getAsLong());
             bridge.insertObjectToDB("aoe_clan",data);
             return BaseResponse.createFullMessageResponse(0, "success", data);
         } catch (Exception e) {
@@ -36,26 +27,15 @@ public class ClanService implements IClanService{
         }
     }
 
-    public JsonObject updateClan(JsonObject json) {
+    public JsonObject updateClan(JsonObject data) {
         try {
             SQLJavaBridge bridge = HikariClients.instance().defaulSQLJavaBridge();
-            long clanId = json.get("id").getAsLong();
-            String clanName = json.get("nick_name").getAsString();
-            String query = "SELECT id from aoe_clan WHERE nick_name = ? AND id != ?";
-            boolean isClanNameExist = bridge.queryExist(query, clanName, clanId);
-            if (isClanNameExist)
-                return BaseResponse.createFullMessageResponse(12, "clan_name_exist");
-            String clanFullName = json.get("full_name").getAsString();
-            String avatar = json.get("avatar").getAsString();
-            long createDay = json.get("create_day").getAsLong();
-            String founder = json.get("founder").getAsString();
-            String ownerUnit = json.get("owner_unit").getAsString();
-            String sport = json.get("sport").getAsString();
-            JsonObject detail = json.get("detail").getAsJsonObject();
-            query = "UPDATE aoe_clan SET nick_name = ?,avatar = ?,create_day = ?,founder = ?,owner_unit = ?,sport = ?,detail = ?,full_name = ? WHERE id = ?";
-            bridge.update(query, clanName, avatar, createDay, founder, ownerUnit, sport, detail, clanFullName, clanId);
-
-            return BaseResponse.createFullMessageResponse(0, "success");
+            int x = bridge.updateObjectToDb("aoe_clan", data);
+            if(x == 0) {
+                return BaseResponse.createFullMessageResponse(10, "update_failure");
+            }else{
+                return BaseResponse.createFullMessageResponse(0, "success");
+            }
         } catch (Exception e) {
             String stacktrace = ExceptionUtils.getStackTrace(e);
             DebugLogger.error(stacktrace);
@@ -71,7 +51,6 @@ public class ClanService implements IClanService{
             //Data return
             String query = "SELECT * from aoe_clan where state = ? LIMIT ? OFFSET ?";
             JsonArray clans = bridge.query(query, 0, recordPerPage, (page - 1) * recordPerPage);
-
             result.add("clans", clans);
             return BaseResponse.createFullMessageResponse(0, "success", result);
         } catch (Exception e) {
